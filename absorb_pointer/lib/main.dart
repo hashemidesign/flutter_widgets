ca9a83954662944f09c9eb7865ef8a0077868389
Mohsen Hashemi <m.hashemi.code@gmail.com>
2023-03-04 10:41:33 +0330

    AbsorbPointer widget

---
+++ absorb_pointer/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AbsorbPointer Widget"),
+      ),
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            SizedBox(
+              width: 200.0,
+              height: 100.0,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: null,
+              ),
+            ),
+            SizedBox(
+              width: 100.0,
+              height: 200.0,
+              child: AbsorbPointer(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue.shade200,
+                  ),
+                  child: null,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
